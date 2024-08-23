9e84cb18f3dc24abf8ad8857323f2466fd0e6174
aksalatdev <73862605+aksalatdev@users.noreply.github.com>
2024-08-23 19:07:36 +0700

    prepare delployment part bingung replace 242 deploy.yml

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const upload = require("../config/multer");
 const imageController = require("../controllers/imageController");
 
-// Definisikan rute dan hubungkan ke controllerrrr
+// Definisikan rute dan hubungkan ke controllerrrrr
 router.post("/upload", upload.single("image"), imageController.uploadImage);
 router.get("/", imageController.getAllImages);
 
