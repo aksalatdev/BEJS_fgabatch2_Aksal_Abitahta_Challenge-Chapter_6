0f303be5612bd990708074d0c0eb9a07b86eaa3b
aksalatdev <73862605+aksalatdev@users.noreply.github.com>
2024-08-23 21:56:24 +0700

    deploy final settingHAHAHAH ELAH DATABASEll

--- config/imagekit.js
+++ config/imagekit.js
@@ -4,7 +4,7 @@ const { IMAGEKIT_PUBLIC_KEY, IMAGEKIT_SECRET_KEY, IMAGEKIT_URL_ENDPOINT } =
 	process.env;
 
 const imagekit = new ImageKit({
-	publicKey: IMAGEKIT_PUBLIC_KEY, // Menggunakan nilai dari process.envd
+	publicKey: IMAGEKIT_PUBLIC_KEY, // Menggunakan nilai dari process.envddsd
 	privateKey: IMAGEKIT_SECRET_KEY, // Menggunakan nilai dari process.env
 	urlEndpoint: IMAGEKIT_URL_ENDPOINT, // Menggunakan nilai dari process.env
 });
