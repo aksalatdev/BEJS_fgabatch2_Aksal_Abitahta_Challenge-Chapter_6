5925e42a140d2327061f6db1c4df196d91886d40
aksalatdev <73862605+aksalatdev@users.noreply.github.com>
2024-08-23 21:39:05 +0700

    deploy final settingHAHAH

--- config/imagekit.js
+++ config/imagekit.js
@@ -1,15 +1,12 @@
-const ImageKit = require('imagekit');
+const ImageKit = require("imagekit");
 
-const {
-  IMAGEKIT_PUBLIC_KEY,
-  IMAGEKIT_SECRET_KEY,
-  IMAGEKIT_URL_ENDPOINT
-} = process.env;
+const { IMAGEKIT_PUBLIC_KEY, IMAGEKIT_SECRET_KEY, IMAGEKIT_URL_ENDPOINT } =
+	process.env;
 
 const imagekit = new ImageKit({
-  publicKey: IMAGEKIT_PUBLIC_KEY,  // Menggunakan nilai dari process.env
-  privateKey: IMAGEKIT_SECRET_KEY,  // Menggunakan nilai dari process.env
-  urlEndpoint: IMAGEKIT_URL_ENDPOINT // Menggunakan nilai dari process.env
+	publicKey: IMAGEKIT_PUBLIC_KEY, // Menggunakan nilai dari process.envd
+	privateKey: IMAGEKIT_SECRET_KEY, // Menggunakan nilai dari process.env
+	urlEndpoint: IMAGEKIT_URL_ENDPOINT, // Menggunakan nilai dari process.env
 });
 
 module.exports = imagekit;
