032dda23919898c8e5a8ba1cc220cbc3f9df3330
aksalatdev <73862605+aksalatdev@users.noreply.github.com>
2024-08-23 20:04:15 +0700

    prepare delployment NGAJAKIN BERANTEM ANJROT AOWKOA

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home pageEeess. */
+/* GET home pageEeess.ss */
 router.get("/", (req, res, next) => {
 	res.render("index", { title: "Expresss" });
 });
