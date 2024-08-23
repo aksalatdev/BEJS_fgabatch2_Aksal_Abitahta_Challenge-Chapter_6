3fa5d3eda2b8a7d4fdb6f27675da60a4edb47dd2
aksalatdev <73862605+aksalatdev@users.noreply.github.com>
2024-08-23 18:27:16 +0700

    prepare delployment part bingung bgttt

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const upload = require("../config/multer");
 const imageController = require("../controllers/imageController");
 
-// Definisikan rute dan hubungkan ke controllerr
+// Definisikan rute dan hubungkan ke controllerrr
 router.post("/upload", upload.single("image"), imageController.uploadImage);
 router.get("/", imageController.getAllImages);
 
