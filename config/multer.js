e6cde4c9bc85496c669c4f1239341b7782d32f38
aksalatdev <73862605+aksalatdev@users.noreply.github.com>
2024-08-23 16:21:50 +0700

    refactor: change multer storage configuration to memoryStorage for file handling

--- config/multer.js
+++ config/multer.js
@@ -1,16 +1,16 @@
-const multer = require('multer');
-const path = require('node:path');
+const multer = require("multer");
+const path = require("node:path");
 
 // setting penyimpanan multer
 const storage = multer.memoryStorage({
-   destination: function (req, file, cb) {
-      cb(null, 'uploads/'); // direktori penyimpanan upload  file
-   },
-   filename: function (req, file, cb) {
-      cb(null, Date.now() + path.extname(file.originalname)); // nama file yang di upload + tanggal
-   }
+	destination: (req, file, cb) => {
+		cb(null, "uploads/"); // direktori penyimpanan upload  file
+	},
+	filename: (req, file, cb) => {
+		cb(null, Date.now() + path.extname(file.originalname)); // nama file yang di upload + tanggal
+	},
 });
 
-const upload = multer ({ storage: storage });
+const upload = multer({ storage: storage });
 
 module.exports = upload;
