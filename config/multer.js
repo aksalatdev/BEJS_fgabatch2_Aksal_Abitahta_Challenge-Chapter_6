01496e159e101671bcfb6ca5d4bbce8826cec089
aksalatdev <73862605+aksalatdev@users.noreply.github.com>
2024-08-23 20:29:20 +0700

    asdasdasdasdsHUAAad

--- config/multer.js
+++ config/multer.js
@@ -4,7 +4,7 @@ const path = require("node:path");
 // setting penyimpanan multer
 const storage = multer.memoryStorage({
 	destination: (req, file, cb) => {
-		cb(null, "uploads/"); // direktori penyimpanan upload  fileesssdaassd
+		cb(null, "uploads/"); // direktori penyimpanan upload  fileesssdaassdssss
 	},
 	filename: (req, file, cb) => {
 		cb(null, Date.now() + path.extname(file.originalname)); // nama file yang di upload + tanggal
