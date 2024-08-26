4fd70b568e65ab19736b856a62cd2293b12b7950
aksalatdev <73862605+aksalatdev@users.noreply.github.com>
2024-08-27 00:49:42 +0700

    feat(api): add Get Image Details endpoint

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,58 +1,73 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const imagekit = require('../config/imagekit');
+const imagekit = require("../config/imagekit");
 
 // upload gambar
 exports.uploadImage = async (req, res) => {
-    try {
-        const { title, description } = req.body;
-        const file = req.file;
+	try {
+		const { title, description } = req.body;
+		const file = req.file;
 
-        // Periksa apakah file ada
-        if (!file) {
-            return res.status(400).json({ error: "No file uploaded" });
-        }
-// console.log('File received:', file);
-//         console.log('File path:', file.path);
-//     console.log('File buffer:', file.buffer);
+		// Periksa apakah file ada
+		if (!file) {
+			return res.status(400).json({ error: "No file uploaded" });
+		}
+		// console.log('File received:', file);
+		//         console.log('File path:', file.path);
+		//     console.log('File buffer:', file.buffer);
 
+		// upload gambar ke imagekit.io
+		const uploadResponse = await imagekit.upload({
+			file: file.buffer,
+			fileName: file.originalname,
+			folder: "/uploads",
+		});
+		//  console.log('ImageKit response:', uploadResponse);
 
-        // upload gambar ke imagekit.io
-        const uploadResponse = await imagekit.upload({
-            file: file.buffer,
-            fileName: file.originalname,
-            folder: '/uploads'
-        });
-        //  console.log('ImageKit response:', uploadResponse);
+		// simpan data gambar ke database
+		const image = await prisma.image.create({
+			data: {
+				title: title,
+				description: description,
+				imageUrl: uploadResponse.url,
+			},
+		});
 
-        // simpan data gambar ke database
-        const image = await prisma.image.create({
-            data: {
-                title: title,
-                description: description,
-                imageUrl: uploadResponse.url,
-            }
-        });
-
-        res.status(201).json({
-            message: 'Upload file sukses',
-            image 
-        });
-
-    } catch (error) {
-        res.status(500).json({
-            error: "Upload file gagal",
-            details: error.message
-        });
-    }
+		res.status(201).json({
+			message: "Upload file sukses",
+			image,
+		});
+	} catch (error) {
+		res.status(500).json({
+			error: "Upload file gagal",
+			details: error.message,
+		});
+	}
 };
 
 // Lihat daftar gambar
 exports.getAllImages = async (req, res) => {
-  try {
-    const images = await prisma.image.findMany();
-    res.json(images);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch images' });
-  }
+	try {
+		const images = await prisma.image.findMany();
+		res.json(images);
+	} catch (error) {
+		res.status(500).json({ error: "Failed to fetch images" });
+	}
+};
+
+// Misal get image details
+exports.getImageDetails = async (req, res) => {
+	const { id } = req.params;
+
+	try {
+		const image = await prisma.image.findUnique({
+			where: { id: Number(id) },
+		});
+		if (!image) {
+			return res.status(404).json({ error: "Gambar Tidak Ditemukan" });
+		}
+		res.status(200).json(image);
+	} catch (error) {
+		res.status(500).json({ error: "Gagal Mengambil Detail Gambar" });
+	}
 };
